60d9062eb37e517cd0d3c462f02b198b918ed4f7
Mlik Mourad Ilyes <dzsi2007@gmail.com>
2017-03-29 16:56:16 +0200

    revert back with some minor changes

--- config.js
+++ config.js
@@ -41,26 +41,26 @@ var config = {
     livenet: {
       provider: 'insight',
       url: 'http://localhost:3030',
-    }
-   // testnet: {
-      //provider: 'insight',
-      //url: 'https://test-insight.bitpay.com:443',
+    },
+    testnet: {
+      provider: 'insight',
+      url: 'https://test-insight.bitpay.com:443',
       // url: 'http://localhost:3001',
       // Multiple servers (in priority order)
       // url: ['http://a.b.c', 'https://test-insight.bitpay.com:443'],
-   // },
+    },
   },
-  pushNotificationsOpts: {
+  /*pushNotificationsOpts: {
     templatePath: './lib/templates',
     defaultLanguage: 'en',
-    defaultUnit: 'grs',
+    defaultUnit: 'btc',
     subjectPrefix: '',
     pushServerUrl: 'http://localhost:8000',
-  }
-  //fiatRateServiceOpts: {
-    //defaultProvider: 'BitPay',
-    //fetchInterval: 60, // in minutes
-  //},
+  }, */
+  fiatRateServiceOpts: {
+    defaultProvider: 'BitPay',
+    fetchInterval: 60, // in minutes
+  },
   // To use email notifications uncomment this:
   // emailOpts: {
   //  host: 'localhost',
